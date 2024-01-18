581c93b4268334a080d798a5f16dba4eade69f85
Andrey <tsariukandreyjs@gmail.com>
2024-01-18 17:40:20 +0300

    add proper imports

--- engine/memory/memory.abstract.ts
+++ engine/memory/memory.abstract.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import { IVAPISerializedTaskMemory } from './memory.types'
-import { IVAPIConnectorVariable, IVAPIMarkupVariable } from 'engine/connectors/connector.types';
+import { IVAPIConnectorVariable, IVAPIMarkupVariable } from '../connectors/connector.types';
 
 export class VAPITaskMemory {
 
