f6bbc075de7e4e46f88081529983a2571a8853c8
Andrey <tsariukandreyjs@gmail.com>
2024-01-23 19:01:14 +0300

    fixing importing issue

--- engine/index.ts
+++ engine/index.ts
@@ -1,57 +1,64 @@
-export { ParamsParser } from './engine/parsers/params.parser'
-export { ParamIterable, ParamsIterator } from './engine/parsers/params.iterator'
+export { ParamsParser } from './parsers/params.parser'
+export { ParamIterable, ParamsIterator } from './parsers/params.iterator'
 // export * as ParsersTypes from './engine/parsers/parser.types'
 
 
 // =======Connectors=======
 // Wrappers on top of protocols e.g. HTTP
-export { VAPIConnector } from './engine/connectors/connector.abstract';
-export * from './engine/connectors/connectors-types';
-export { VAPIConnectorError } from './engine/connectors/connector.errors';
-export { VAPIConnectorsManager } from './engine/connectors/connector.manager';
+export { VAPIConnector } from './connectors/connector.abstract';
+export * from './connectors/connectors-types';
+export { VAPIConnectorError } from './connectors/connector.errors';
+export { VAPIConnectorsManager } from './connectors/connector.manager';
+import {
+    IVAPIConnectorVariable,
+    IVAPIMarkupVariable,
+    VAPIConnectorParamsMappingObject,
+    IVAPIConnectorExecuteAddons
+} from './connectors/connector.types';
+
 export {
     IVAPIConnectorVariable,
     IVAPIMarkupVariable,
     VAPIConnectorParamsMappingObject,
     IVAPIConnectorExecuteAddons
-} from './engine/connectors/connector.types';
+}
 
 
 // =======Execution=======
 // Control on top of the execution process, routing, connections, etc. 
-export { VAPIExecutionManager } from './engine/execution/execution.manager';
-export { VAPIExecutionError } from './engine/execution/execution.error';
-export * as EXECUTION_CONSTANTS from './engine/execution/execution.constants';
-export * as VAPIExecutionTypes from './engine/execution/execution.types';
+export { VAPIExecutionManager } from './execution/execution.manager';
+export { VAPIExecutionError } from './execution/execution.error';
+export * as EXECUTION_CONSTANTS from './execution/execution.constants';
+export * as VAPIExecutionTypes from './execution/execution.types';
 
 
 // =======Loggers=======
 // A wrapper on top of possible loggers and listeners
-export { VAPILogger } from './engine/logger/logger.abstract';
-export { VAPILoggerError } from './engine/logger/logger.errors';
-export * as VAPILoggerTypes from './engine/logger/logger.types';
+export { VAPILogger } from './logger/logger.abstract';
+export { VAPILoggerError } from './logger/logger.errors';
+export * as VAPILoggerTypes from './logger/logger.types';
 
 
 // =======Task=======
 // A wrapper on top of possible loggers and listeners
-export { VAPIExecutionTask } from './engine/tasks/task.abstract';
-export { VAPIExecutionTaskError } from './engine/tasks/task.errors';
-export * as TASK_CONSTANTS from './engine/tasks/task.constants';
+export { VAPIExecutionTask } from './tasks/task.abstract';
+export { VAPIExecutionTaskError } from './tasks/task.errors';
+export * as TASK_CONSTANTS from './tasks/task.constants';
 export {
     IVAPIExecutionTaskConstructorConfig,
     IVAPISerializedExecutionTask,
     IVAPIExecutionTaskOnProps,
     IVAPIExecutionTaskEmitProps
-} from './engine/tasks/task.types';
+} from './tasks/task.types';
 
 
 // =======Errors=======
-export { VAPIError } from './engine/error/error.abstract'
+export { VAPIError } from './error/error.abstract'
 
 
 // =======Memory=======
-export { VAPITaskMemory } from './engine/memory/memory.abstract'
-export { IVAPISerializedTaskMemory } from './engine/memory/memory.types'
+export { VAPITaskMemory } from './memory/memory.abstract'
+export { IVAPISerializedTaskMemory } from './memory/memory.types'
 
 
 
@@ -59,4 +66,4 @@ export { IVAPISerializedTaskMemory } from './engine/memory/memory.types'
 
 export {
     Dictionary as VAPIDictionary
-} from './engine/shared/types'
+} from './shared/types'
